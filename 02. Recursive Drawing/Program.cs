824a03f09784243d77d328f912acb636003642f0
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-08-05 17:43:32 +0300

    update

---
+++ 02. Recursive Drawing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _02._Recursive_Drawing
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var size = int.Parse(Console.ReadLine());
+            var arr = new int[size];
+
+            FigurePrint(arr, arr.Length);
+        }
+
+        static void FigurePrint(int[] arr, int index)
+        {
+            if (index == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine(new string('*', index));
+
+            FigurePrint(arr, index - 1);
+
+            Console.WriteLine(new string('#', index));
+        }
+    }
+}
