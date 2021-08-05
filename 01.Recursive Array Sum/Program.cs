84f56d1c26466dc58f144f15b46f5a935618d278
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-08-05 17:42:58 +0300

    update

---
+++ 01.Recursive Array Sum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _1_Recursive_Array_Sum
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(CalcArr(arr, 0));
+        }
+
+        public static int CalcArr(int[] arr, int index)
+        {
+            if (index == arr.Length)
+            {
+                return 0;
+            }
+
+            return arr[index] + CalcArr(arr, index + 1);
+        }
+    }
+}
