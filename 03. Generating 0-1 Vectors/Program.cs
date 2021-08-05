b3d701875d619fc3bd9f4ce4fac39207411139fc
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-08-05 17:43:51 +0300

    update

---
+++ 03. Generating 0-1 Vectors/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _03._Generating_0_1_Vectors
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var num = int.Parse(Console.ReadLine());
+            var arr = new int[num];
+            Generate(arr, 0); 
+        }
+
+        public static void Generate(int[] arr, int index)
+        {
+            if (index == arr.Length)
+            {
+                Console.WriteLine(string.Join("", arr));
+                return;
+            }
+
+            for (int i = 0; i <= 1; i++)
+            {
+                arr[index] = i;
+                Generate(arr, index + 1);
+            }
+        }
+    }
+}
---
+++ 04. Recursive Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04._Recursive_Factorial
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var num = int.Parse(Console.ReadLine());
+            Console.WriteLine(FactorialCalc(num));
+        }
+
+        public static int FactorialCalc(int num)
+        {
+            if (num == 0)
+            {
+                return 1;
+            }
+
+            return num * FactorialCalc(num - 1);
+        }
+    }
+}
