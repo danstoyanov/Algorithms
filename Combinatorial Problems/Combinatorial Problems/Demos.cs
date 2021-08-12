20a2a9d2e44407f2792c47f3a054ba995a7232b7
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-08-12 17:43:59 +0300

    update

---
+++ Combinatorial Problems/Combinatorial Problems/Demos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Combinatorial_Problems
+{
+    public class Program
+    {
+        private static string[] currElements;
+        private static int[] permutations;
+        private static bool[] isValid;
+
+        static void Main()
+        {
+             currElements = Console.ReadLine()
+             .Split()
+             .ToArray();
+
+             permutations = new int[currElements.Length];
+             isValid = new bool[currElements.Length];
+
+            Permute(0);
+        }
+
+        private static void Permute(int index)
+        {
+            if(index >= currElements.Length)
+            {
+                Console.WriteLine(string.Join(" ", permutations));
+                return;
+            }
+            
+        }
+    }
+}
