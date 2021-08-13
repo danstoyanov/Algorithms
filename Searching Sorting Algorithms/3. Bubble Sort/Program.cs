3e8396247b10f8ddf0b469441ad122e5a973aec2
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-08-13 18:10:54 +0300

    update

--- Searching Sorting Algorithms/3. Bubble Sort/Program.cs
+++ Searching Sorting Algorithms/3. Bubble Sort/Program.cs
@@ -3,7 +3,7 @@ using System.Linq;
 
 namespace _3._Bubble_Sort
 {
-    public class BubbleSort
+    public class Bubble_Sort
     {
         static void Main()
         {
@@ -17,7 +17,30 @@ namespace _3._Bubble_Sort
 
         private static void BubbleSort(int[] inputArr)
         {
+            var isSorted = false;
+            var i = 0;
 
+            while (!isSorted)
+            {
+                isSorted = true;
+
+                for (int j = 1; j < inputArr.Length - i; j++)
+                {
+                    if (inputArr[j - 1] > inputArr[j])
+                    {
+                        Swap(inputArr, j - 1, j);
+                        isSorted = false;
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", inputArr));
+        }
+        private static void Swap(int[] array, int currIndex, int minIndex)
+        {
+            var temp = array[currIndex];
+            array[currIndex] = array[minIndex];
+            array[minIndex] = temp;
         }
     }
 }
