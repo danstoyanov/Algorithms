9cec963da13d6e86a1da1020969f60923049a16c
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-08-13 13:49:56 +0300

    update

---
+++ Combinatorial Problems/1. Permutations without Repetitions/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Permutations_without_Repetitions
+{
+    public class Program
+    {
+        public static string[] elements;
+        public static string[] permutations;
+        public static bool[] currElement;
+
+        static void Main()
+        {
+            elements = Console
+                .ReadLine()
+                .Split()
+                .ToArray();
+
+            permutations = new string[elements.Length];
+            currElement = new bool[elements.Length];
+
+            Permute(0);
+        }
+        private static void Permute(int index)
+        {
+            if (index >= elements.Length)
+            {
+                Console.WriteLine(string.Join(" ", permutations));
+                return;
+            }
+
+            for (int elementIndx = 0; elementIndx < elements.Length; elementIndx++)
+            {
+                if (!currElement[elementIndx])
+                {
+                    currElement[elementIndx] = true;
+                    permutations[index] = elements[elementIndx];
+                    Permute(index + 1);
+                    currElement[elementIndx] = false;
+                }
+            }
+        }
+    }
+}
